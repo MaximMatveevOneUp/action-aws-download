92d2f258f8e968413adac107a6452149a428f8ad
Maxim Matveev <maxim.m@oneupgames.gg>
2023-05-29 13:24:32 +0300

    Exit with aws exit code

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,4 +12,8 @@ aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 aws s3 cp s3://${AWS_S3_BUCKET}${AWS_S3_PATH} ${FILE} \
             --region ${AWS_REGION} $*
 
+status=$?
+
 rm -rf ~/.aws
+
+exit $status
