1886e680b0915690f5a607db5dd4ea791e304fbc
Maxim Matveev <maxim.w.matveev@gmail.com>
2023-06-23 13:01:59 +0300

    Custom params

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
 aws s3 cp s3://${AWS_S3_BUCKET}${AWS_S3_PATH} ${FILE} \
-            --region ${AWS_REGION} $*
+            --region ${AWS_REGION} ${AWS_CUSTOM_PARAMS} $*
 
 status=$?
 
